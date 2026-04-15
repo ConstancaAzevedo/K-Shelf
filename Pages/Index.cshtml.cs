293847e5f61166fe89af57530fa743f7d39a79c5
const <constancaazevedo3@gmail.com>
2026-04-15 10:09:20 +0100

    Modificação do layout da página principal e da navbar

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,13 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using K_Shelf.Data;
+using K_Shelf.Models;
 
 namespace K_Shelf.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
 
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int ArtistasCount { get; set; }
+        public int AlbunsCount { get; set; }
+        public int ColecoesCount { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            ArtistasCount = await _context.Artistas.CountAsync();
+            AlbunsCount = await _context.Albuns.CountAsync();
+            ColecoesCount = await _context.Colecoes.CountAsync();
         }
     }
 }
