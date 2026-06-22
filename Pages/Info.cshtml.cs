aab44fb401042ee688eb39eece4bbd15bb79c5fe
const <constancaazevedo3@gmail.com>
2026-06-23 00:32:13 +0100

    Adicionar página de Informações do projeto e link na navbar

---
+++ Pages/Info.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace K_Shelf.Pages
+{
+    public class InfoModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Página estática - não precisa de lógica
+        }
+    }
+}
